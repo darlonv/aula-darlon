5de2b4a2634662efde963409017fc921a60ec976
Darlon <darlonv@gmail.com>
2023-08-31 01:38:12 +0000

    scripts atualizados

--- git_setenv.sh
+++ git_setenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git config --global --add safe.directory /src
 git config --global user.email ${GIT_EMAIL}
 git config --global user.name ${GIT_NAME}
 git config credential.https://github.com.username ${GIT_LOGIN}
